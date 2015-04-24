fb9d97e705059520ce2847ed98e4bb4d000db250
Andy <Andy@Andrew>
2015-04-23 21:38:34 -0500

    tercera correccion

--- PruebaGit/src/pruebagit/PruebaGit.java
+++ PruebaGit/src/pruebagit/PruebaGit.java
@@ -19,7 +19,7 @@ public class PruebaGit {
         
         System.out.println("primera linea de codigo de prueba ");
         System.out.println("primera modificacion de archivos");
-        System.out.println("hola modificacion desde github")
+        System.out.println("hola modificacion desde github");
     }
     
 }
